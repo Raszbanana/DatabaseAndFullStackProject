9d3da99201d2effcaf4395b2866d2078f1efc6bd
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2023-12-18 14:31:35 +0100

    added collection for booking schema

--- backend/api/src/mongoose-models/booking/booking.schema.ts
+++ backend/api/src/mongoose-models/booking/booking.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema()
+@Schema({ collection: 'bookings'})
 export class BookingMongooseModel {
   @Prop()
   bookingId: number;
